9162a2405d3d593e660e68dc75933e5e89f67556
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-22 14:59:27 +0200

    Update radio.py

--- radio.py
+++ radio.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import importlib
 
 st.set_page_config(page_title="File Selector")
 
@@ -12,20 +11,14 @@ selected_file = st.radio("Select a file to execute:", ["date_guess.py", "selecte
 
 # Execute the selected file when the "Run" button is clicked
 if st.button("Run"):
-    module_name = selected_file.replace(".py", "")
-    try:
-        module = importlib.import_module(module_name)
-        function_name = "generate_news" if module_name == "date_guess" else "calculate_specific_date_magic"
-        selected_function = getattr(module, function_name)
+    if selected_file == "date_guess.py":
+        # Execute code from date_guess.py
+        exec(open("date_guess.py").read())
+    elif selected_file == "selected_day.py":
+        # Execute code from selected_day.py
+        exec(open("selected_day.py").read())
 
-        if module_name == "date_guess":
-            selected_date = selected_function()
-            news_summary = generate_news(selected_date)
-            st.header("Please verify, but according to ChatGPT in that period... ")
-            st.write(news_summary)
-        elif module_name == "selected_day":
-            specific_date = st.date_input("Select a specific date:")
-            if specific_date:
-                selected_function(specific_date)
-    except Exception as e:
-        st.error(f"Error executing the script: {e}")
+
+
+
+
