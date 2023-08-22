4debd316da3c3345b15e8515ac14d6188ac7598c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-22 16:31:44 +0200

    Update test_radio.py

--- test_radio.py
+++ test_radio.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.set_page_config(page_title = "This is a Multipage WebApp")
-st.title("This is the Home Page Geeks.")
+st.title("This is the Home Page Geeks")
 st.sidebar.success("Select Any Page from here")
