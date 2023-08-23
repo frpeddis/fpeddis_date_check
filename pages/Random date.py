f806816374d8b76bd4b7da00c0e8d67aa7ae93fa
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 22:21:27 +0200

    Update Random date.py

--- pages/Random date.py
+++ pages/Random date.py
@@ -127,7 +127,7 @@ if check_button:
             st.markdown("""
             <style>
             .big-font {
-                font-size:300px !important;
+                font-size:260px !important;
             }
             </style>
             """, unsafe_allow_html=True)
