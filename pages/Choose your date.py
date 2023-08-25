d6ba7400e0d6dae258b6317c54ab50f5c6fca180
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-25 20:46:36 +0200

    Update Choose your date.py

--- pages/Choose your date.py
+++ pages/Choose your date.py
@@ -1,116 +1,41 @@
-import random
-import calendar
-import streamlit as st
-from datetime import datetime, timedelta
-import openai
-import time
-import streamlit.components.v1 as com
-import requests
-from PIL import Image
-from io import BytesIO
-import pandas as pd
 
-# Checkbox to toggle image display
-
-openai.api_key = st.secrets["API_KEY"]
-st.set_page_config(page_title="🌀 WeekDay Whiz")
-
-def generate_news(selected_date):
-    prompt = f"What happens on {selected_date}?\nGive me a good news simply with an initial 😄, a neutral news simply with an initial 😐, and a bad news simply with an initial 😔. Do not mention if it is good, neutral or bad news, just use the icons. Do not mention any date in your answer. jump a line forevery news. Insert related Wikipedia links."
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=200,
-        temperature=0.7
-    )
-
-    return response.choices[0].text.strip()
-
-
-
-# Streamlit app title
-st.title(":sunglasses: What day is it? Random date 🎲")
-show_images = st.checkbox("Show me how to calculate ! ")
-
-if show_images:
-    image_links = [
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_1.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_2.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_3.jpeg",
-        "https://raw.githubusercontent.com/frpeddis/TestApp1/1ce97d47cedac010c814496ef6e34773a748cff6/MAGIC%20DAY%20CALCULATOR_4.jpeg"    ]
-    
-    for i, link in enumerate(image_links):
-        response = requests.get(link)
-        img = Image.open(BytesIO(response.content))
-        st.image(img, use_column_width=True)
-
-
-# Function to calculate a random date
-def calculate_random_date():
-    start_date = datetime(1582, 10, 15)
-    end_date = datetime(2099, 12, 31)
-    return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
-
-# Check if random_date and start_time are in session state, if not, calculate and store them
-if 'random_date' not in st.session_state:
-    st.session_state.random_date = calculate_random_date()
-    
-
-if 'start_time' not in st.session_state:
-    st.session_state.start_time = datetime.now()
-
-if 'check_pressed' not in st.session_state:
-    st.session_state.check_pressed = False
-
-if 'time_taken' not in st.session_state:
-    st.session_state.time_taken = 0
-
-# Display the date in the format dd-mmm-yyyy
-description = "**Random Date:**"
-value = "**" + st.session_state.random_date.strftime("%d-%b-%Y") + "**"
-st.markdown(f"{description} {value}")
-#st.write("**Random Date:**", st.session_state.random_date.strftime("%d-%b-%Y"))
-selected_date = st.session_state.random_date.strftime("%d-%b-%Y")
-#com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
-
-
-# Calculate time taken
-if not st.session_state.check_pressed:
-    time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
-    display_time_taken = False
+# Create a datetime object based on user input
+if not invalid_date:
+    selected_date = datetime(selected_year, selected_month, selected_day)
+    # Display the selected date in bold and with formatting
+    st.markdown(f"**Selected Date:** {selected_date.strftime('%d-%b-%Y')}", unsafe_allow_html=True)
+    # Get the day of the week for the selected date
+    day_of_week = calendar.day_name[selected_date.weekday()]
 else:
-    time_taken = st.session_state.time_taken
-    display_time_taken = True
+    st.markdown("<font color='red'>Invalid date</font>", unsafe_allow_html=True)
 
-# Prompt the user to select the day of the week from a dropdown list
-selected_day_of_week = st.selectbox("Select the day of the week:", list(calendar.day_name))
+# Prompt the user to select the expected day of the week from a dropdown list
+expected_day_of_week = st.selectbox("Expected day of the week?:", list(calendar.day_name))
 
 # Add a "Check" button to confirm the selection
 check_button = st.button("Check")
 
-
-
-if check_button:
-    st.session_state.check_pressed = True
-
-    # Confirm the day of the week selected by the user
-    day_of_week = calendar.day_name[st.session_state.random_date.weekday()]
-
-    if selected_day_of_week == day_of_week:
-        st.balloons()
+if check_button and not invalid_date:
+    # Compare the user-selected day of the week with the actual day of the week
+    if day_of_week == expected_day_of_week:
         st.success(day_of_week + " is OK! :thumbsup:")
-                
+        st.balloons()
+        #news_summary = generate_news(selected_date)
+        #st.title("Please verify, but according to ChatGPT that day...")
+        #st.write(news_summary)
     else:
+        #st.error(day_of_week + " ...sorry for that!!!")
         st.error(day_of_week + " is the right day! :coffee: That's why...")
         com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
-                # Step 1: User selects a date
-        selected_date = st.session_state.random_date
+        #st.snow()
+        #value = "**" + selected_date + "**"
+        #selected_date = st.session_state.random_date
         
         if selected_date:
+            value = "**" + selected_date.strftime('%d-%b-%Y') + "**"
+
             description2 = "Focus on "
-            st.markdown(f"{description2} {value}")
-           
+            st.markdown(f"{description2} {value}")           
 
             # Step 2: Take the last 2 digits of the year
             year_last_2_digits = selected_date.year % 100
@@ -151,10 +76,10 @@ if check_button:
             calculated_string = f"**:green[{year_last_2_digits}]** + **:green[{year_divided_by_4}]** + **:green[{century_correction_value}]** + **:green[{month_coefficient}]** + **:green[{day_of_month}]**"
             st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
             
+            
             #calculated_string = f"{year_last_2_digits} + {year_divided_by_4} + {century_correction_value} + {month_coefficient} + {day_of_month}"
             #st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
             #st.write("Magic Sum: ", calculated_string, " = ", subtotal)
-            
             
             # Step 2: Take the last 2 digits of the year (continued)
             st.write(year_last_2_digits, ": Last 2 digits of the year YY")
@@ -174,6 +99,11 @@ if check_button:
             # Step 7: Divide the subtotal by 7 and find the remainder (continued)
             #st.write(":point_right: Remainder after dividing ", subtotal, "  by 7:", remainder)
             st.write(":point_right: Remainder after dividing the Magic Sum ", subtotal, "  by 7 ---> ", f"<span style='font-size:18px; font-weight:bold;'>{remainder}</span>", unsafe_allow_html=True)
+
+
+
+
+
             
             # Display Correspondence Table
             #st.write("Correspondence between Remainders and Days of the Week Table:")
@@ -228,16 +158,8 @@ if check_button:
             #st.write("Month Coefficient Table:")
             st.dataframe(df_month_coefficients)
 
-    # Calculate time taken to make the selection
-    st.session_state.time_taken = (datetime.now() - st.session_state.start_time).total_seconds()
-    time_taken = st.session_state.time_taken
-    display_time_taken = True
+        #news_summary = generate_news(selected_date)
+        #st.title("Please verify, but according to ChatGPT that day...")
+        #st.write(news_summary)
 
-# Show the amount of seconds taken
-if display_time_taken:
-    st.write(":hourglass: Time taken to check:", round(time_taken, 2), "seconds")
-
-    if st.button("In that period..."):
-            news_summary = generate_news(selected_date)
-            st.header("Beware of hallucinations, but according to Chatgpt at that time...")
-            st.write(news_summary)
+  
