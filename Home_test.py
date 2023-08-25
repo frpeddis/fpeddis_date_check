6cabf12d024115284f55c3acdee219b443b3988e
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-25 20:41:27 +0200

    Update and rename weekwhiz.py to Home_test.py

---
+++ Home_test.py
@@ -0,0 +1,18 @@
+#Author: Francesco Peddis with ChatGPT
+
+import streamlit as st
+
+
+
+st.set_page_config(page_title="🌀 Guess the Weekday!")
+
+st.write("↖️ Your Calendar Challenge on the upper left sidebar")
+
+st.title("Welcome!!!  :sunglasses:")
+st.header("🗓️ Guess the **weekday** for **any date!**")
+st.write("You just need simple basic math and a little bit of memory... ") 
+st.write("Let's give it a try!")
+
+
+ 
+st.sidebar.success("Select your challenge ")
