e3ad5dd49bc6ebafc8bbadf8f6d925539bf103d6
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-25 21:42:38 +0200

    Update Home_test.py

--- Home_test.py
+++ Home_test.py
@@ -10,6 +10,7 @@ st.write("↖️ Your Calendar Challenge on the upper left sidebar")
 
 st.title("Welcome!!!  :sunglasses:")
 st.header("🗓️ Guess the **weekday** for **any date!**")
+com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
 st.write("You just need simple basic math and a little bit of memory... ") 
 st.write("Let's give it a try!")
 
