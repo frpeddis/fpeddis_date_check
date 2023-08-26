4b08f29d34d3d33d7b20d9213c3cad3744d82ac6
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-26 10:39:20 +0200

    Update Home_test.py

--- Home_test.py
+++ Home_test.py
@@ -11,9 +11,9 @@ st.subheader("↖️ Your Calendar Challenge on the upper left sidebar")
 com.iframe("https://lottie.host/?file=5a47a9d0-e7b0-492a-a32c-dd53d0fbfd5b/NaMYqV899B.json")
 
 st.title("Welcome!!!  :sunglasses:")
-st.header("Guess the **weekday** for **any date!**")
+#st.header("Guess the **weekday** for **any date!**")
 
-text = "With a little practice you can calculate it in your head..."
+text = "Guess the **weekday** for **any date!**"
 
 
 t = st.empty()
@@ -21,7 +21,7 @@ for i in range(len(text) + 1):
     t.markdown("## %s" % text[0:i])
     time.sleep(0.1)
 
-#st.subheader("With a little practice you can calculate it in your head... ") 
+st.subheader("With a little practice you can calculate it in your head... ") 
 
 
  
