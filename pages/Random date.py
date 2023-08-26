16ac975f97504270abe7f44388a77567bb110557
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-26 15:21:05 +0200

    Update Random date.py

--- pages/Random date.py
+++ pages/Random date.py
@@ -174,7 +174,7 @@ if check_button:
             
             # Display calculated string
             time.sleep(2)
-            calculated_string = f"**:green[{year_last_2_digits}]** + **:green[{year_divided_by_4}]** + **:green[{century_correction_value}]** + **:green[{month_coefficient}]** + **:green[{day_of_month}]**"
+            calculated_string = f"**:green[{year_last_2_digits}]** + **:green[{year_divided_by_4}]** + **:green[{century_correction_value}]** + **:green[{month_coefficient}]** + **:green[{day_of_month}]** = **:red[{subtotal}]**"
             st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
             st.write(":point_right: Magic Sum: ", calculated_string, " = ", sbt)
 
