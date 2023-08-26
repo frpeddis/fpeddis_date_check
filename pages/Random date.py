7c3ec15afa929b730d3dfc99690c934919a5057f
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-26 11:23:32 +0200

    Update Random date.py

--- pages/Random date.py
+++ pages/Random date.py
@@ -114,6 +114,7 @@ if check_button:
         if selected_date:
             #description2 = "Focus on "
             #st.markdown(f"{description2} {value}")
+            st.markdown(f"{description2}")
 
 
             text = value
