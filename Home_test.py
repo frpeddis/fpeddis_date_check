64a124a8d9f53e38dba96e15b9f150dc48841f0c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-27 17:27:02 +0200

    Update Home_test.py

--- Home_test.py
+++ Home_test.py
@@ -10,7 +10,7 @@ st.subheader("↖️ Your Calendar Challenge on the upper left sidebar")
 
 #com.iframe("https://lottie.host/?file=5a47a9d0-e7b0-492a-a32c-dd53d0fbfd5b/NaMYqV899B.json")
  
-com.iframe("https://lottie.host/?file=90eeda85-77c8-40c8-8ca0-975987488dd9/p88GhLxUZM.json")
+com.iframe("https://lottie.host/?file=f8b3d68c-9de2-4a01-8c29-1bb24bbe72c6/Rafbgzfeae.json")
 
 st.title("Welcome!!!  :sunglasses:")
 
